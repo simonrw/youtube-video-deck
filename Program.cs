8aaff4ec3d42aa79629852cbeebe28a5c513d6f8
Simon Walker <s.r.walker101@googlemail.com>
2019-09-25 00:30:09 +0100

    Initial dotnet project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace youtube_video_deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
