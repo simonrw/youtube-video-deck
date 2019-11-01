d812d31c9d9bf8e09365818f666f156ceda176fa
Simon Walker <s.r.walker101@googlemail.com>
2019-11-01 20:02:34 +0000

    Update request to include client

--- request.py
+++ request.py
@@ -10,24 +10,28 @@ import dotenv
 username = "outsidexbox"
 
 
-memory = Memory(location="/tmp")
+memory = Memory("/tmp")
 
-@memory.cache
-def fetch_channel_info(key, username):
-    # Get the uploads url
-    url = "https://www.googleapis.com/youtube/v3/channels?part={part}&key={key}&forUsername={username}".format(
-            part="contentDetails", key=key, username=username)
-    r = requests.get(url)
-    r.raise_for_status()
-    return r.json()
 
-@memory.cache
-def fetch_items(key, uploads_id):
-    url = "https://www.googleapis.com/youtube/v3/playlistItems?key={key}&part=contentDetails&maxResults=50&playlistId={playlist_id}".format(
-        key=key, playlist_id=uploads_id)
-    r = requests.get(url)
-    r.raise_for_status()
-    return r.json()
+class Client:
+    def __init__(self, key):
+        self.key = key
+
+
+    def fetch_channel_info(self, username):
+        url = "https://www.googleapis.com/youtube/v3/channels?part={part}&key={key}&forUsername={username}".format(
+                part="contentDetails", key=self.key, username=username)
+        r = requests.get(url)
+        r.raise_for_status()
+        results = r.json()
+        return results["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+    def fetch_items(self, uploads_id):
+        url = "https://www.googleapis.com/youtube/v3/playlistItems?key={key}&part=contentDetails&maxResults=50&playlistId={playlist_id}".format(
+            key=self.key, playlist_id=uploads_id)
+        r = requests.get(url)
+        r.raise_for_status()
+        return r.json()
 
 
 if __name__ == "__main__":
@@ -35,7 +39,7 @@ if __name__ == "__main__":
 
     key = os.environ["GOOGLE_API_KEY"]
 
-    result = fetch_channel_info(key, username)
-    uploads_id = result["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+    client = Client(key)
 
-    pprint.pprint(fetch_items(key, uploads_id))
+    uploads_id = client.fetch_channel_info(username)
+    pprint.pprint(client.fetch_items(uploads_id))
