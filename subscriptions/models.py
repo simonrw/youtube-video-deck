e163768bd1c7cf0b96733049b42cbc628fdfa812
Simon Walker <s.r.walker101@googlemail.com>
2019-11-04 12:58:45 +0000

    Create initial Subscription class

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+    """
+    Represents a single subscription for a user
+
+    *Responsibilities*
+
+    * links the subscription ID to a series of videos
+    * tracks the last time the subscription was checked for new items
+    * stores whether the subscription is a channel or playlist
+    """
+
+    name = models.CharField(max_length=255)
+    last_checked = models.DateTimeField(null=True)
