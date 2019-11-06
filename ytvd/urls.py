65e2511d9cbffd9d9f2fa734bbb9756ac683b38e
Simon Walker <s.r.walker101@googlemail.com>
2019-11-06 21:29:15 +0000

    Put app at root dir

--- ytvd/urls.py
+++ ytvd/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("ytvd/", include("subscriptions.urls")),
+    path("", include("subscriptions.urls")),
 ]
