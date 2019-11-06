8462980122e310975eed8d3c58e8ec0adfe03611
Simon Walker <s.r.walker101@googlemail.com>
2019-11-06 23:08:10 +0000

    Update admin page to reduce fields available

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Subscription, Video
 
-admin.site.register(Subscription)
-admin.site.register(Video)
+
+class VideoAdmin(admin.ModelAdmin):
+    fields = ["name", "watched"]
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    fields = ["user", "name", "last_checked"]
+
+
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(Video, VideoAdmin)
